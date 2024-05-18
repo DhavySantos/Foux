88baaca513c2b14372929b7098800c6cc7887639
DhavyLTS <dhavy.s@proton.me>
2024-05-18 17:30:34 -0300

    feat: impl framerate system to window.

--- src/core/window.rs
+++ src/core/window.rs
@@ -1,11 +1,14 @@
 extern crate glfw;
 
+use std::time::Duration;
+
 use glfw::{Context, OpenGlProfileHint::Core, WindowEvent};
 use glfw::{Glfw, GlfwReceiver, PWindow};
 
 type Events = GlfwReceiver<(f64, WindowEvent)>;
 
 pub struct Window {
+    target_framerate: f64,
     window: PWindow,
     events: Events,
     glfw: Glfw,
@@ -28,31 +31,49 @@ impl Window {
         window.make_current();
         window.set_key_polling(true);
 
-        unsafe {
-            gl::Viewport(0, 0, 600, 400);
-        }
-
         Self {
+            target_framerate: 60.0,
             window,
             events,
             glfw,
         }
     }
 
-    pub fn should_close(&self) -> bool {
+    pub(crate) fn set_vsync(&mut self, value: bool) {
+        let state = if value { 1 } else { 0 };
+        self.glfw.set_swap_interval(glfw::SwapInterval::Sync(state));
+    }
+
+    pub(crate) fn set_title(&mut self, title: &str) {
+        self.window.set_title(title);
+    }
+
+    pub(crate) fn target_frametime(&self) -> Duration {
+        Duration::from_secs_f64(1.0 / self.target_framerate)
+    }
+
+    pub(crate) fn set_target_framerate(&mut self, target_framerate: f64) {
+        self.target_framerate = target_framerate;
+    }
+
+    pub(crate) fn target_framerate(&self) -> f64 {
+        self.target_framerate
+    }
+
+    pub(crate) fn should_close(&self) -> bool {
         self.window.should_close()
     }
 
-    pub fn set_should_close(&mut self, value: bool) {
+    pub(crate) fn set_should_close(&mut self, value: bool) {
         self.window.set_should_close(value);
     }
 
-    pub fn update(&mut self) {
+    pub(crate) fn update(&mut self) {
         self.window.swap_buffers();
         self.glfw.poll_events();
     }
 
-    pub fn clear(&self) {
+    pub(crate) fn clear(&self) {
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT);
             gl::ClearColor(0.0, 0.0, 0.0, 1.0);
