77fd07c57aeb7d596c8ace9c0f99047e4a911352
Dhavy Santos <dhavy.s@proton.me>
2024-11-17 16:49:43 -0300

    feat: Implement get/set `should_close` methods

--- src/core/window.rs
+++ src/core/window.rs
@@ -1,11 +1,15 @@
 pub struct Window {
+    should_close: bool,
     title: String,
 }
 
 impl Window {
     pub fn new(title: &str) -> Window {
         let title = String::from(title);
-        Window { title }
+        Window {
+            should_close: false,
+            title,
+        }
     }
 
     pub fn get_title(&self) -> &str {
@@ -14,5 +18,13 @@ impl Window {
 
     pub fn set_title(&mut self, title: &str) {
         self.title = String::from(title);
+    }
+
+    pub fn should_close(&self) -> bool {
+        self.should_close
+    }
+
+    pub fn set_should_close(&mut self, state: bool) {
+        self.should_close = state;
     }
 }
