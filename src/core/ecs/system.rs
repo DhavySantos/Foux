1eff87a809793a8adcd24742d332265b6104f0b6
Dhavy Santos <dhavy.s@proton.me>
2024-11-21 02:42:08 -0300

    feat: Update `Scheduler` to use `MeshRenderer` and pass it to system functions
    - Modified `Scheduler` to store and invoke systems that accept `Window`
    and `Vec<MeshRenderer>` as arguments.
    
    - Updated `System` type and the `insert` and `invoke` methods to handle
    `MeshRenderer`.

--- src/core/ecs/system.rs
+++ src/core/ecs/system.rs
@@ -1,3 +1,4 @@
+use crate::core::{components::MeshRenderer, Window};
 use std::collections::HashMap;
 
 /// Enum representing different system types for scheduling.
@@ -7,13 +8,17 @@ pub enum SystemType {
     Startup,
     /// Represents the update system type.
     Update,
+    /// Represents the key system type.
+    Key,
 }
+
+pub type System = dyn FnMut(&mut Window, &mut Vec<MeshRenderer>);
 
 /// A struct that holds a collection of systems (functions) categorized by their type.
 #[derive(Default)]
 pub struct Scheduler {
     /// A HashMap that maps system types to a vector of systems (functions).
-    systems: HashMap<SystemType, Vec<Box<dyn FnMut()>>>,
+    systems: HashMap<SystemType, Vec<Box<System>>>,
 }
 
 impl Scheduler {
@@ -37,7 +42,7 @@ impl Scheduler {
     /// let mut scheduler = Scheduler::new();
     /// scheduler.insert(SystemType::Startup, || println!("Starting up..."));
     /// ```
-    pub fn insert(&mut self, system_type: SystemType, system: impl FnMut() + 'static) {
+    pub fn insert(&mut self, system_type: SystemType, system: impl FnMut(&mut Window, &mut Vec<MeshRenderer>) + 'static) {
         self.systems
             .entry(system_type)
             .or_default()
@@ -51,6 +56,7 @@ impl Scheduler {
     /// # Arguments
     ///
     /// * `system_type` - The type of system whose functions are to be invoked.
+    /// * `window` - The window instance passed to the systems.
     ///
     /// # Example
     /// ```
@@ -58,10 +64,12 @@ impl Scheduler {
     /// scheduler.insert(SystemType::Startup, || println!("Starting up..."));
     /// scheduler.invoke(SystemType::Startup);
     /// ```
-    pub fn invoke(&mut self, system_type: SystemType) {
-        for system in self.systems.entry(system_type).or_default() {
-            system();
+    pub fn invoke(&mut self, system_type: SystemType, window: &mut Window, renders: &mut Vec<MeshRenderer>) {
+        if let Some(systems) = self.systems.get_mut(&system_type) {
+            for system in systems.iter_mut() {
+                // Dereference the Box to invoke the function
+                system(window, renders);
+            }
         }
     }
 }
-
