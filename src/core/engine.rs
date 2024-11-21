4e3289fde6ad4f12ab6631275ed42ee3f04fb5cd
Dhavy Santos <dhavy.s@proton.me>
2024-11-20 23:25:11 -0300

    feat: add scheduler struct to engine

--- src/core/engine.rs
+++ src/core/engine.rs
@@ -1,12 +1,24 @@
+use crate::core::ecs::{Scheduler, SystemType};
 use crate::core::Window;
 
-pub struct Engine {}
+#[derive(Default)]
+pub struct Engine {
+    scheduler: Scheduler,
+}
 
 impl Engine {
+    pub fn add_system(&mut self, system_type: SystemType, system: impl FnMut() + 'static) {
+        self.scheduler.insert(system_type, system);
+    }
+
     pub fn run(&mut self) {
-        let mut window = Window::new("Foux Engine", 800, 640);
+        self.scheduler.invoke(SystemType::Startup);
+
+        let mut window = Window::new("Foux Engine", 800, 480);
+
         while !window.should_close() {
-            window.clear(0.3, 0.7, 0.3, 1.0);
+            window.clear(0.07, 0.17, 0.07, 1.0);
+            self.scheduler.invoke(SystemType::Update);
             window.update();
         }
     }
