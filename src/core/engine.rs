9dc86377e3bf1de189a250402ec2bb49f13a8b02
Dhavy Santos <dhavy.s@proton.me>
2024-11-21 02:42:37 -0300

    feat: Modify `Engine` to handle `MeshRenderer` in system functions
    - Updated `Engine`'s `add_system` and `run` methods to pass `Window` and
    `Vec<MeshRenderer>` to system functions.
    
    - Ensured that `MeshRenderer` instances are rendered within the main
    loop.

--- src/core/engine.rs
+++ src/core/engine.rs
@@ -1,24 +1,29 @@
 use crate::core::ecs::{Scheduler, SystemType};
 use crate::core::Window;
 
+use super::components::MeshRenderer;
+
 #[derive(Default)]
 pub struct Engine {
     scheduler: Scheduler,
+    renders: Vec<MeshRenderer>
 }
 
 impl Engine {
-    pub fn add_system(&mut self, system_type: SystemType, system: impl FnMut() + 'static) {
+    pub fn add_system(&mut self, system_type: SystemType, system: impl FnMut(&mut Window, &mut Vec<MeshRenderer>) + 'static) {
         self.scheduler.insert(system_type, system);
     }
 
     pub fn run(&mut self) {
-        self.scheduler.invoke(SystemType::Startup);
-
         let mut window = Window::new("Foux Engine", 800, 480);
+        self.scheduler.invoke(SystemType::Startup, &mut window, &mut self.renders);
 
         while !window.should_close() {
             window.clear(0.07, 0.17, 0.07, 1.0);
-            self.scheduler.invoke(SystemType::Update);
+            self.scheduler.invoke(SystemType::Update, &mut window, &mut self.renders);
+            for render in self.renders.iter() {
+                render.render();
+            }
             window.update();
         }
     }
