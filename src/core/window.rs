a695290e752ae3caf27ca2323e0b59a41ba295fc
Dhavy Santos <dhavy.s@proton.me>
2024-11-20 23:24:27 -0300

    fix: wrong gl clear call order

--- src/core/window.rs
+++ src/core/window.rs
@@ -73,8 +73,8 @@ impl Window {
     /// This function calls OpenGL directly, which is unsafe.
     pub fn clear(&self, red: f32, green: f32, blue: f32, alpha: f32) {
         unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
             gl::ClearColor(red, green, blue, alpha);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
         }
     }
 
